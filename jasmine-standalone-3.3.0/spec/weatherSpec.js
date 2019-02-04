4ff961f3c3ce246a606cb77a1c966d293922bf29
isabelcooper <isabel@triptease.com>
2019-02-04 17:31:03 +0000

    empty weather files

---
+++ jasmine-standalone-3.3.0/spec/weatherSpec.js
@@ -0,0 +1,12 @@
+// describe( function() {
+//
+//   var weather;
+//
+//   beforeEach( function() {
+//     weather = new Weather
+//   });
+//
+//   it( "should return random weather", function() {
+//     expect(weather.random()).toEqual("Sunny")
+//   });
+// });
